17b24d6fa7fdc1779d41475fbb0c911adea0d089
wukachn <peter.hamerrr@gmail.com>
2024-04-21 13:11:20 +0100

    Load font into memory.

--- src/main/java/io/github/wukachn/Memory.java
+++ src/main/java/io/github/wukachn/Memory.java
@@ -2,15 +2,36 @@ package io.github.wukachn;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class Memory {
 
+  private static int FONT_OFFSET = 80;
   private static int PROGRAM_OFFSET = 512;
   private static int MEMORY_SIZE = 4096;
-
-  byte[] MEMORY = new byte[MEMORY_SIZE];
+  private static byte[] FONT = {
+      (byte) 0xF0, (byte) 0x90, (byte) 0x90, (byte) 0x90, (byte) 0xF0, // 0
+      (byte) 0x20, (byte) 0x60, (byte) 0x20, (byte) 0x20, (byte) 0x70, // 1
+      (byte) 0xF0, (byte) 0x10, (byte) 0xF0, (byte) 0x80, (byte) 0xF0, // 2
+      (byte) 0xF0, (byte) 0x10, (byte) 0xF0, (byte) 0x10, (byte) 0xF0, // 3
+      (byte) 0x90, (byte) 0x90, (byte) 0xF0, (byte) 0x10, (byte) 0x10, // 4
+      (byte) 0xF0, (byte) 0x80, (byte) 0xF0, (byte) 0x10, (byte) 0xF0, // 5
+      (byte) 0xF0, (byte) 0x80, (byte) 0xF0, (byte) 0x90, (byte) 0xF0, // 6
+      (byte) 0xF0, (byte) 0x10, (byte) 0x20, (byte) 0x40, (byte) 0x40, // 7
+      (byte) 0xF0, (byte) 0x90, (byte) 0xF0, (byte) 0x90, (byte) 0xF0, // 8
+      (byte) 0xF0, (byte) 0x90, (byte) 0xF0, (byte) 0x10, (byte) 0xF0, // 9
+      (byte) 0xF0, (byte) 0x90, (byte) 0xF0, (byte) 0x90, (byte) 0x90, // A
+      (byte) 0xE0, (byte) 0x90, (byte) 0xE0, (byte) 0x90, (byte) 0xE0, // B
+      (byte) 0xF0, (byte) 0x80, (byte) 0x80, (byte) 0x80, (byte) 0xF0, // C
+      (byte) 0xE0, (byte) 0x90, (byte) 0x90, (byte) 0x90, (byte) 0xE0, // D
+      (byte) 0xF0, (byte) 0x80, (byte) 0xF0, (byte) 0x80, (byte) 0xF0, // E
+      (byte) 0xF0, (byte) 0x80, (byte) 0xF0, (byte) 0x80, (byte) 0x80  // F
+  };
+  private static byte[] MEMORY = new byte[MEMORY_SIZE];
 
   public Memory(String romPath) {
+    loadFont();
     loadProgram(romPath);
   }
 
@@ -27,4 +48,7 @@ public class Memory {
     }
   }
 
+  private void loadFont() {
+    System.arraycopy(FONT, 0, MEMORY, FONT_OFFSET, FONT.length);
+  }
 }
